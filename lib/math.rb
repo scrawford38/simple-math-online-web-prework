f0bdc3e77f57e4bd02b22a779a1064a1d147c201
Sally Crawford <sally.crawford@gmail.com>
2019-04-07 18:06:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,10 +15,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  
+  sum=num1%num2  
 end
 
 def square_root(num)
+  sum=math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
